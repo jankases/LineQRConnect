64437f5e3ef5dcd191e91f4553f12c6da9bd36e0
jankases <surasak.jankasem@gmail.com>
2026-09-18 15:26:50 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,8 +3,8 @@
 // ==========================================
 
 // กุญแจสาธารณะ (Publishable Key) สำหรับฝั่ง Frontend ปลอดภัยที่จะเปิดเผย
-const SUPABASE_URL = 'https://trrfwszgscftnybhhsor.supabase.co'; // เปลี่ยนเป็น URL ของคุณ
-const SUPABASE_KEY = 'sb_publishable_SyOGO6gN7DGYmj0ltcvokg_y3WOtZdH'; // เปลี่ยนเป็น Key ของคุณ
+const SUPABASE_URL = 'https://zxkwkyujrekwvieocawm.supabase.co'; // เปลี่ยนเป็น URL ของคุณ
+const SUPABASE_KEY = 'sb_publishable_IuqevzRo0vu0Ky33F_DBHg_oAErQsvm'; // เปลี่ยนเป็น Key ของคุณ
 
 // สร้างตัวเชื่อมต่อ Database ให้ทุกหน้าจอสามารถเรียกใช้งานได้
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
